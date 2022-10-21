4b925cd28ed76ed611b90918a5df3c1755fc50cf
yedu <yedukrishna269@gmail.com>
2022-10-21 14:04:16 +0530

    updated index.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Happy Diwali!!!!!\n');
+  res.send('Hello World!!!!!\n');
 });
 
 app.listen(PORT);
